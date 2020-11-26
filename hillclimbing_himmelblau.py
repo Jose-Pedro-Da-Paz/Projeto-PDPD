393b9c8ee3ab4c74d8f4210891aac93871fdfeb5
José Pedro Da Paz Ferreira <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2020-11-26 16:20:56 -0300

    Create hillclimbing_himmelblau.py

---
+++ hillclimbing_himmelblau.py
@@ -0,0 +1,57 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+def funcao(x, n):
+    y = (((x[0]**2)+x[1]-11)**2)+((x[0]+(x[1]**2)-7)**2)
+    return y
+
+
+def v_random_float(n,cont):
+    for i in range(0,n):   
+        xajuste[i] = random.gauss(0,(0.99**cont))
+    return xajuste
+
+
+
+n = int(input("Quantas dimensões a função terá: "))
+
+x = np.zeros(n)
+xajuste = np.zeros(n)
+
+for i in range(0,n):
+    x[i] = float(input("Digite um suposto valor para x{}: ".format(i)))
+
+
+xmelhor = x
+ymelhor = funcao(xmelhor,n)
+
+cont = 0
+grafico = []
+
+
+
+while ymelhor != 0:
+      
+    x = xmelhor + v_random_float(n,cont)
+
+    y = funcao(x,n)
+
+    if ymelhor > y:
+        xmelhor = x
+        ymelhor = y
+
+    
+    grafico.append(ymelhor)
+    
+    plt.plot(np.log(grafico), color='red')
+   
+    cont += 1
+    if cont == 2000:
+        print('Acabou as tentativas')
+        break
+    print('O melhor valor de y = {} para x = {}, {}ºtentativa'.format(ymelhor,xmelhor,cont))
+    '''print('O valor atual de y = {} para x = {}'.format(y,x))'''
+print('FIM!') 
+plt.show()
