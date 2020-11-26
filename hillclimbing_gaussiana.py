8a5149a3672e849ced323a611e47bf64f72ab802
José Pedro Da Paz Ferreira <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2020-11-26 15:31:54 -0300

    Update hillclimbing_gaussiana.py

--- hillclimbing_gaussiana.py
+++ hillclimbing_gaussiana.py
@@ -3,61 +3,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
+def funcao(x, n):
+    y = np.dot(x,x)
+    return y
 
 
-x1 = float(input("Digite um suposto valor para x1: "))
-x2 = float(input("Digite um suposto valor para x2: "))
-x3 = float(input("Digite um suposto valor para x3: "))
+def v_random_float(n,cont):
+    for i in range(0,n):   
+        xajuste[i] = random.gauss(0,(0.99**cont))
+    return xajuste
 
-xmelhor1 = x1
-xmelhor2 = x2
-xmelhor3 = x3
 
-x = np.array([x1, x2, x3])
-xajuste = np.array([x1, x2, x3])
-y0 = x**2
-y = y0[0]+y0[1]+y0[2]
-xmelhor = np.array([xmelhor1, xmelhor2, xmelhor3])
 
-ymelhor = xmelhor[0]**2 + xmelhor[1]**2 + xmelhor[2]**2
+n = int(input("Quantas dimensões a função terá: "))
+
+x = np.zeros(n)
+xajuste = np.zeros(n)
+
+for i in range(0,n):
+    x[i] = float(input("Digite um suposto valor para x{}: ".format(i)))
+
+
+xmelhor = x
+ymelhor = funcao(xmelhor,n)
 
 cont = 0
 grafico = []
 
 
-def v_random_float():
-    for i in range(0,3):   
-        xajuste[i] = random.gauss(0,(0.99**cont))
 
 while ymelhor != 0:
-    
-    
-    v_random_float() 
-    
-    for i in range(0,3):
-        x[i] = xmelhor[i] + xajuste[i]
+      
+    x = xmelhor + v_random_float(n,cont)
 
-    y0 = x**2
-    y = y0[0]+y0[1]+y0[2]
+    y = funcao(x,n)
 
     if ymelhor > y:
-        for j in range(0,3):
-            xmelhor[j] = x[j]
-        ymelhor = xmelhor[0]**2 + xmelhor[1]**2 + xmelhor[2]**2
+        xmelhor = x
+        ymelhor = y
 
     
     grafico.append(ymelhor)
     
     plt.plot(np.log(grafico), color='red')
    
-    
-    
-
     cont += 1
     if cont == 2000:
         print('Não consegui achar o valor, a resolução é um Nº Complexo')
         break
     print('O melhor valor de y = {} para x = {}, {}ºtentativa'.format(ymelhor,xmelhor,cont))
-    print('O valor atual de y = {} para x = {}'.format(y,x))
+    '''print('O valor atual de y = {} para x = {}'.format(y,x))'''
 print('FIM!') 
 plt.show()
