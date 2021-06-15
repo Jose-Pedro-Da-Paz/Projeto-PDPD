607ebb755184c3b10936f8cdac563f34c3f90d81
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2021-06-15 20:10:59 -0300

    simulated annealing applied to a resistor bridge

---
+++ Pyspice/ahkab_sim_annealing_resistor.py
@@ -0,0 +1,153 @@
+import ahkab
+import bokeh
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+num = 2 # Determina a quantidade de "espaços" que serão criados nos vetores
+cont = 1 # contador
+r = np.random.rand(num)
+rajuste = np.zeros(num)
+k = 0
+
+# Simulated Annealing
+n = 50 # Número de Cíclos
+m = 50 # Número de tentativas por ciclo
+na = 0.0 # Número de soluções aceitas
+p1 = 0.7 # Probabilidade de ser aceito uma solução pior no começo do algoritmo
+p50 = 0.001 # Probabilidade de ser aceito uma solução pior no final do algoritmo
+t1 = -1.0/math.log(p1) # "Temperatura inicial"
+t50 = -1.0/math.log(p50) # "Temperatura final"
+frac = (t50/t1)**(1.0/(n-1.0)) # redução fracionada que ocorre por ciclo
+
+
+def entrada_valor(num):
+    """
+    entrada_valor tem como função receber os valores que o usuário 
+    insere. E armazena em um vetor xinicio, este vetor determina 
+    qual será o ponto de início de procura do algoritmo pelo melhor valor 
+    que soluciona a função custo.
+
+    Args:
+        num ([int]): número de váriaves da função custo
+
+    Returns:
+        [float]: Retorna o vetor que indica qual é o ponto de ínicio do algoritmo
+    """
+    xinicio = np.zeros(num)
+    for k in range(num):
+        xinicio[k] = float(input("x{} ínicio: ".format(k)))
+    return xinicio 
+
+
+def simula_circuito(resistor1, resistor2): 
+    """
+    
+
+    Args:
+        resistor1 ([float]): [description]
+        resistor2 ([float]): [description]
+
+    Returns:
+        [float]: [description]
+    """
+    mycir = ahkab.Circuit('Divisor de Tensão')
+
+    mycir.add_vsource('V1', 'n1', mycir.gnd, dc_value=10)
+    mycir.add_resistor('R1', 'n1', 'n2', value = resistor1)
+    mycir.add_resistor('R2', 'n2', mycir.gnd, value = resistor2)
+
+
+    opa = ahkab.new_op(outfile='teste')
+    r = ahkab.run(mycir, opa)['op']
+    resultado = r.asarray()
+    
+    return resultado[1]
+
+def funcao_custo(resistor1, resistor2):
+    tensao = simula_circuito(resistor1, resistor2)
+    return ((tensao - 5)**2)
+    
+
+
+
+# Inicia x
+x = np.zeros((n+1,num))
+x[0] = entrada_valor(num)
+xi = np.zeros(num)
+xi = x[0]
+na = na + 1.0
+
+# melhor resultado para x
+xc = np.zeros(num)
+xc = np.copy(x[0])
+fc = funcao_custo(xi[0], xi[1])
+fs = np.zeros(n+1)
+fs[0] = fc
+
+# temperatura atual
+t = t1
+pior = 0
+
+# DeltaE médio
+DeltaE_avg = 0.0
+for i in range(n):
+    print('Ciclo: ' + str(i) + ' Temperatura: ' + str(t))
+    
+    for j in range(m):
+        # gera novos pontos para ser testados
+        
+        for k in range(num):
+            xi[k] = xc[k] + random.random() - 0.5
+            # limita máximos e minimos onde serão procurados as respostas
+            xi[k] = max(min(xi[k],5.0),-5.0)
+        
+        DeltaE = abs(funcao_custo(xi[0], xi[1])-fc)
+        
+        if (funcao_custo(xi[0], xi[1])>fc):
+            # Inicializa DeltaE_avg se encontrar uma soluçao pior
+            #   Na primeira iteração
+            
+            if (i==0 and j==0): DeltaE_avg = DeltaE
+            # Função objetivo é pior
+            # gera a probabilidade de aceitar ou não a solução pior
+            p = math.exp(-DeltaE/(DeltaE_avg * t))
+            # determina se aceita o pior ponto
+            
+            if (random.random()<p):
+                # aceita a pior solução
+                accept = True
+                pior += 1
+            else:
+                # Não aceita a pior solução
+                accept = False
+
+        else:
+            # se a função objetivo for menor automaticamente aceita
+            accept = True
+        
+        if (accept==True):
+            # Atualiza as novas soluções aceitas 
+            
+            for k in range(num):
+                xc[k] = xi[k]
+            fc = funcao_custo(xc[0], xc[1])
+            # incrementa o número de soluções aceitas
+            na = na + 1.0
+            # Atualiza DeltaE_avg
+            DeltaE_avg = (DeltaE_avg * (na-1.0) +  DeltaE) / na
+    # Guarda os melhores valores de x no fim de cada ciclo
+    
+    for k in range(num):
+        x[i+1][k] = xc[k]
+    fs[i+1] = fc
+    # diminui a "temperatura" para o próximo ciclo
+    t = frac * t
+
+# printa as soluções
+print('Best solution: ' + str(xc))
+print('Best objective: ' + str(fc))
+
+
